5a4a858447444ad22e125f2baa162912a9fa7357
abdul2926 <90007948+abdul2926@users.noreply.github.com>
2021-12-30 13:22:37 +0100

    replace 4 spaces with 1 tab

--- server/error.ts
+++ server/error.ts
@@ -1,44 +1,44 @@
 import * as http from 'http';
 
 export function serveError(code: number, response: http.ServerResponse) {
-    switch (code) {
-        case 404:
-            serveHTML(code, 'Not found', response);
-            break;
-        case 413:
-            serveHTML(code, 'Request entity too large', response);
-            break;
-        case 400:
-            serveHTML(code, 'Bad Request', response);
-            break;
-        case 401:
-            serveHTML(code, 'Unauthorized', response);
-            break;
-        case 403:
-            serveHTML(code, 'Forbidden', response);
-            break;
-        case 405:
-            serveHTML(code, 'Method not allowed', response);
-            break;
-        default:
-            serveHTML(500, 'Internal server error', response);
-            break;
-    }
+	switch (code) {
+		case 404:
+			serveHTML(code, 'Not found', response);
+			break;
+		case 413:
+			serveHTML(code, 'Request entity too large', response);
+			break;
+		case 400:
+			serveHTML(code, 'Bad Request', response);
+			break;
+		case 401:
+			serveHTML(code, 'Unauthorized', response);
+			break;
+		case 403:
+			serveHTML(code, 'Forbidden', response);
+			break;
+		case 405:
+			serveHTML(code, 'Method not allowed', response);
+			break;
+		default:
+			serveHTML(500, 'Internal server error', response);
+			break;
+	}
 }
 
 function serveHTML(code: number, elaboration: string, response: http.ServerResponse) {
-    response.writeHead(code, {
-        'Content-Type': 'text/html'
-    });
-    response.write(getErrorHTML(code, elaboration));
-    response.end();
+	response.writeHead(code, {
+		'Content-Type': 'text/html'
+	});
+	response.write(getErrorHTML(code, elaboration));
+	response.end();
 }
 
 function getErrorHTML(code: number, elaboration: string): string {
-    return `
-    <html>
-        <body>
-            <h1> ${code}: ${elaboration} </h1>
-        </body>
-    </html>`;
+	return `
+	<html>
+		<body>
+			<h1> ${code}: ${elaboration} </h1>
+		</body>
+	</html>`;
 }
--- server/requests.ts
+++ server/requests.ts
@@ -5,70 +5,70 @@ import * as fs from 'fs';
 import { serveError } from './error';
 
 export function handleRequest(request: http.IncomingMessage, response: http.ServerResponse) {
-    var path = url.parse(request.url, true).pathname;
-    var contentType = 'text/html';
-    
-    var pathSplit = path.split('.');
-    var filetype = pathSplit[pathSplit.length -1];
+	var path = url.parse(request.url, true).pathname;
+	var contentType = 'text/html';
+	
+	var pathSplit = path.split('.');
+	var filetype = pathSplit[pathSplit.length -1];
 
-    if (filetype) {
-        switch (filetype.toLowerCase()) {
-            case 'css':
-                contentType = 'text/css';
-                break;
-            case 'js':
-                contentType = 'text/javascript'
-                break;
-        }
-    }
+	if (filetype) {
+		switch (filetype.toLowerCase()) {
+			case 'css':
+				contentType = 'text/css';
+				break;
+			case 'js':
+				contentType = 'text/javascript'
+				break;
+		}
+	}
 
-    if (contentType != 'text/html') {
-        fs.readFile(`./web${path}`, function (err, data) {
-            if (err) {
-                serveError(404, response);
-            } else {
-                response.writeHead(200, {
-                    'Content-Type': contentType
-                });
-                response.write(data);
-                response.end();
-            }
-        });
-        return;
-    }
+	if (contentType != 'text/html') {
+		fs.readFile(`./web${path}`, function (err, data) {
+			if (err) {
+				serveError(404, response);
+			} else {
+				response.writeHead(200, {
+					'Content-Type': contentType
+				});
+				response.write(data);
+				response.end();
+			}
+		});
+		return;
+	}
 
-    var direct: string;
-    switch (path) {
-        case '/':
-            direct = 'index.html';
-            break;
-        case '/settings':
-            direct = 'settings.html';
-            break;
-        
-        case '/authenticate':
-            direct = 'auth.html';
-            break;
+	var direct: string;
+	switch (path) {
+		case '/':
+			direct = 'index.html';
+			break;
+		case '/settings':
+			direct = 'settings.html';
+			break;
+		
+		case '/authenticate':
+			direct = 'auth.html';
+			break;
 
-        case '/updatehash':
-            direct = 'updatehash.html';
-            break;
-        
-        default:
-            serveError(404, response);
-            return;
-    }
+		case '/updatehash':
+			direct = 'updatehash.html';
+			break;
+		
+		default:
+			serveError(404, response);
+			return;
+	}
 
-    fs.readFile(`./web/${direct}`, function (err, data) {
-        if (err) {
-            serveError(404, response);
-            return;
-        }
+	fs.readFile(`./web/${direct}`, function (err, data) {
+		if (err) {
+			serveError(404, response);
+			return;
+		}
 
-        response.writeHead(200, {
-            'Content-Type': contentType
-        });
-        response.write(data);
-        response.end();
-    });
+		response.writeHead(200, {
+			'Content-Type': contentType
+		});
+		response.write(data);
+		response.end();
+	});
 }
